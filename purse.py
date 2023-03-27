c41476be510ee80ee7b9dce32f115465159fe6a7
EbXpJ6bp <ebxpj6bp@outlook.jp>
2023-03-28 01:58:14 +0900

    tqdmを管理

--- purse.py
+++ purse.py
@@ -4,7 +4,6 @@ import os
 from tqdm import tqdm
 
 
-
 if __name__ == "__main__":
 
     # フォルダー内の全てのフォルダーを取得
@@ -14,19 +13,19 @@ if __name__ == "__main__":
     # フォルダーを日付順にソート
     folders.sort()
 
+    # 未処理のフォルダーを検索
+    folders = [f for f in os.listdir(current_dir) if f.startswith("Results_")]
 
-    for folder in folders:
-        if folder.startswith("Results_"):
-            # フォルダー名から日付を取得
-            folder_path = os.path.join(current_dir, folder)
+    for folder in tqdm(folders, desc='Progress in purse'):
+        # 処理に使うパスを管理
+        folder_path = os.path.join(current_dir, folder)
+        origin_folder_path = os.path.join(folder_path, "origin")
+        comma_folder_path = os.path.join(folder_path, "comma")
 
-            origin_folder_path = os.path.join(folder_path, "origin")
-            comma_folder_path = os.path.join(folder_path, "comma")
+        # 変換と、抽出を実行
+        _convert.semicolon_to_comma(origin_folder_path, comma_folder_path)
+        _extract.hom_interference_experiment_result(folder_path, comma_folder_path)
 
-            _convert.semicolon_to_comma(origin_folder_path, comma_folder_path)
-
-            _extract.hom_interference_experiment_result(folder_path, comma_folder_path)
-
-            new_path = os.path.join(current_dir, f"Done_{folder}")
-            os.rename(folder_path, new_path)
-
+        # 処理が終わったファイルの名前を変更
+        new_path = os.path.join(current_dir, f"Done_{folder}")
+        os.rename(folder_path, new_path)
